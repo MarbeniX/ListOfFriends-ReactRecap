9b9a52b4240f504e5719589923134e8089b931ba
Berlin Ortega <marlitros200002@gmail.com>
2025-08-04 13:58:11 -0600

    Key prop
    Making the SplitBillForm different whenever we click on a different friend

--- src/App.js
+++ src/App.js
@@ -53,6 +53,7 @@ export default function App() {
                 <SplitBill
                     selectedFriend={selectedFriend}
                     onHandleSplitBill={handleSplitBill}
+                    key={selectedFriend.id}
                 />
             )}
         </div>
